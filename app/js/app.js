4c1523eb5a0cc9bed3d5bb2839077c160b337a70
Blake Burnette <bburnette@regencyop.com>
2015-10-08 14:40:57 -0400

    Farm Bureau - Centered Categories

---
+++ app/js/app.js
@@ -0,0 +1,4 @@
+'use strict';
+
+var four51 = {};
+four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'ui.bootstrap', 'angulartics', 'angulartics.google.analytics', 'ngAutocomplete', 'tabsView', 'OrderCloud-ProductMatrix', 'OrderCloud-SpecForms', 'OrderCloud-BackToTop', 'OrderCloud-Carousel', 'OrderCloud-ProductZoom', 'OrderCloud-ContentModal']);
